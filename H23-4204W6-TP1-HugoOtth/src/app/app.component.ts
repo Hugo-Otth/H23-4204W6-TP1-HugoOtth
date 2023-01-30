5bbc0fcd503787967b8bdde32c16671658d4a6db
Hugo Otth <1968678@cegepmontpetit.ca>
2023-01-30 14:45:17 -0500

    CONFIG Copie des fichiers du labo 2

--- H23-4204W6-TP1-HugoOtth/src/app/app.component.ts
+++ H23-4204W6-TP1-HugoOtth/src/app/app.component.ts
@@ -1,3 +1,5 @@
+import { lastValueFrom } from 'rxjs';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'H23-4204W6-TP1-HugoOtth';
+  
+  // Deux variables devront être ajoutées ici
+  result = false;
+  artist : string = "";
+  similarArtists : string[] = [];
+  // Le constructeur devra être ajouté ici
+  constructor(public http : HttpClient){}
+
+  searchArtist():void{
+    this.request();
+    this.result = true;
+  }
+	// La requête HTTP devra être ajoutée ici
+	async request() : Promise<void>{
+    let input = await lastValueFrom(this.http.get<any>("http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=" + this.artist + "&api_key=e34ebf8561ba7c653a21d1d99a1a0070&format=json"));
+    this.similarArtists = [];
+    for(let artist of input.similarartists.artist){
+      this.similarArtists.push(artist.name);
+    }
+  }
+
+  newSearch():void{
+    this.result = false;
+  }
 }
