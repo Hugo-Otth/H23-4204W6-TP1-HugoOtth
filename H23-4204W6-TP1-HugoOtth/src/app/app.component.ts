6de9b6ea3fb4da15da390b7aa12fa00e1e508c20
Hugo Otth <1968678@cegepmontpetit.ca>
2023-01-30 14:22:39 -0500

    PROGRES Cération Angular

---
+++ H23-4204W6-TP1-HugoOtth/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'H23-4204W6-TP1-HugoOtth';
+}
